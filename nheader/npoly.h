efc12b7513a27ded65f6e085c46e45286aee833d
Scstechr <teufelkonig@gmail.com>
2018-03-28 22:50:34 +0900

    while statment changed to for statement

--- nheader/npoly.h
+++ nheader/npoly.h
@@ -12,23 +12,21 @@ typedef struct{
 	int end;
 } nrand_Poly;
 
-nlist_List *npoly_init(double *coeff, int coeff_size, int d_check){
-	nlist_List *poly= (nlist_List*)malloc(sizeof(nlist_List));
+nlist_List *npoly_init(double *coeff, int coeff_size, int d_check)
+{
+	nlist_List *poly = nlist_init();
 	for(int i = 0; i < coeff_size; i++){
 		poly->data[i].ditem = coeff[i];
 		nlist_append(poly, INT_MAX); 
 	}
-	int i = poly->start;
 	int j = 0;
 	double sum = 0.0;
-	while(1){
+	for(int i = poly-> start; i != INT_MAX; i = poly->data[i].after){
 		assert(j <= poly->size);
 		if (poly->data[i].ditem != 0.0){
 			assert(poly->data[i].ditem > 0);
 			sum += poly->data[i].ditem;
 		}
-		i = poly->data[i].after;
-		if(i == INT_MAX) break; 
 		j++;
 	}
 	if (d_check == 0) assert(sum == 1.0);
@@ -38,11 +36,10 @@ nlist_List *npoly_init(double *coeff, int coeff_size, int d_check){
 void npoly_print(nlist_List *poly)
 {
 	assert( poly->size > 1);
-	int i = poly->start;
 	int j = 0; // Safety
 	int k = 0;
 	printf("f(x)=");
-	while(1){
+	for(int i = poly->start; i != INT_MAX; i = poly->data[i].after){
 		assert(j <= poly->size);
 		if (poly->data[i].ditem != 0.0){
 			if(k == 0 ){
@@ -56,8 +53,6 @@ void npoly_print(nlist_List *poly)
 				printf("%+.3lfx^{%d}", poly->data[i].ditem, j);
 			}
 		}
-		i = poly->data[i].after;
-		if(i == INT_MAX) break; 
 		j++;
 	}
 	printf("\n");
@@ -65,17 +60,12 @@ void npoly_print(nlist_List *poly)
 
 double npoly_subs(nlist_List *poly, double num)
 {
-	int i = poly->start;
-	int j = 0;
-	int k = 0;
-	double dnum = 0.0;
-	while(1){
+	int j = 0; int k = 0; double dnum = 0.0;
+	for(int i = poly->start; i != INT_MAX; i = poly->data[i].after){
 		assert(j <= poly->size);
 		if (poly->data[i].ditem != 0.0){
-			dnum += poly->data[i].ditem * pow(num, j);
+			dnum += poly->data[i].ditem * pow(num, j); 
 		}
-		i = poly->data[i].after;
-		if (i == INT_MAX) break;
 		j++;
 	}
 	return dnum;
@@ -91,16 +81,14 @@ void npoly_copy(nlist_List *poly1, nlist_List *poly2){
 void npoly_deriv(nlist_List *poly){
 	assert( poly->size > 1 );
 	double dnum = 0.0; double coef[poly->size];
-	for(int l = 0; l < poly->size; l++) coef[l] = 0.0; 
+	for(int i = 0; i < poly->size; i++) coef[i] = 0.0; 
 	nlist_List *ret = npoly_init(coef, poly->size, 1);
 	
-	int i = poly->start; int j = 0; int k = 0;
-	while(1){
+	int j = 0; int k = 0;
+	for(int i = poly->start; i != INT_MAX; i = poly->data[i].after){
 		assert(j <= poly->size);
 		k = i;
 		if (poly->data[i].ditem != 0.0) dnum += poly->data[i].ditem; 
-		i = poly->data[i].after;
-		if (i == INT_MAX) break;
 		ret->data[k].ditem = i*poly->data[i].ditem;
 		j++;
 	}
