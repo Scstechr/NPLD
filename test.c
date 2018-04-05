f150b4d1316e382072925ebf3850bb88e6dcc1fb
Scstechr <teufelkonig@gmail.com>
2018-04-05 12:44:41 +0900

    test.c changed

--- test.c
+++ test.c
@@ -1,95 +1,56 @@
 #include "header.h"
-#include "../headerfiles/fstack.h"
 
-#define SLOTSIZE 10
-
-//typedef struct{
-//	nlist_List *list;
-//	int before;
-//	int after;
-//} ndict_Item;
-//
-//typedef struct{
-//	ndict_Item data[STACK_SIZE];
-//	int end;
-//	int start;
-//	int size;
-//} ndict_Dict;
-
-ndict_Dict *setUserlist(int usernum){
-	double coef[9];
-	int size = sizeof(coef)/sizeof(double);
-	double_init(coef, size);
-	//coef[2] = 0.5; coef[3] = 0.22; coef[8] = 0.28;
-	coef[2] = 1.0;
-	nlist_List *poly = npoly_init(coef, size, 0);
-	ndict_Dict *userlist = ndict_init();
-	for(int i = 0; i < usernum; i++){
-		nlist_List *list = nrand_pick(SLOTSIZE, nrand_dist_pick(poly));
-		ndict_append(userlist, list);
-	}
-	return userlist;
+void list_ver(){
+	int size = 100;
+	nlist_List *list = nlist_range(7);
+	nlist_insert(list, 0, 1);
+	int index = nlist_index(list, 0);
+	printf("index:%d\n", index);
+	nlist_print(list);
+	nlist_List *rand_list = nrand_pick(100, 8);
+	nlist_print(rand_list);
+	free(list);
+	free(rand_list);
 }
 
-ndict_Dict *setSlotstate(ndict_Dict *userlist){
-	ndict_Dict *slotstate = ndict_range(SLOTSIZE);
-	for(int i = userlist->start;
-			i != INT_MAX; 
-			i = userlist->data[i].after){
-		for(int j = userlist->data[i].list->start;
-				j != INT_MAX;
-				j = userlist->data[i].list->data[j].after){
-			int slot = userlist->data[i].list->data[j].item;
-			nlist_append(slotstate->data[slot].list, i);
-			int sum = slotstate->data[slot].list->data[0].item;
-			nlist_substitute(slotstate->data[slot].list, 0, sum + 1);
-		}
+void dict_ver(){
+	ndict_Dict *dict = ndict_init();
+	int rep[10] = {2, 3, 8, 2, 3, 9, 2, 2, 3, 8};
+	for(int i = 0; i < 10; i ++){
+		nlist_List *list = nrand_pick(100, rep[i]);
+		ndict_append(dict, list);
 	}
-	return slotstate;
 }
 
-void zigzag(ndict_Dict *userlist, ndict_Dict *slotstate){
-	nlist_List *decoded = nlist_init();
-	int progress = 0;
-	for(int i = 1; i < userlist->size; i++){
-		nlist_List *list = userlist->data[i].list;
-		for(int j = userlist->data[i].list->start;
-				j != INT_MAX;
-				j = userlist->data[i].list->data[j].after){
-			if(slotstate->data[list->data[j].item].list->data[0].item == 1){
-				nlist_append(decoded, i); progress ++; break;
-				ndict_delete(slotstate, i);
-			}
-		}
+void nrand_ver(){
+	double coef[9] = {0.0, 0.0, 0.5, 0.28, 0.0, 0.0, 0.0, 0.0, 0.22};
+	int coef_size = sizeof(coef)/sizeof(double);
+	nlist_List *poly = npoly_init(coef, coef_size, 0);
+	ndict_Dict *dict = ndict_init();
+	nrand_dist_check(poly);
+	for(int i = 0; i < 30; i ++){
+		nlist_List *list = nrand_pick(1000, nrand_dist_pick(poly));
+		ndict_append(dict, list);
 	}
-	if(progress > 0){
-		for(int i = decoded->start; i != INT_MAX; i = decoded->data[i].after){
-			ndict_delete(userlist, decoded->data[i].item);
-			for(int j = userlist->data[decoded->data[i].item].list->start;
-					j != INT_MAX;
-					j = userlist->data[decoded->data[i].item].list->data[j].after){
-				slotstate->data[ userlist->data[
-						decoded->data[i].item ].list->data[j].item ].list->data[0].item --;
-			}
-		}
-		nlist_clear(decoded);
-	} 
+	ndict_print(dict);
 }
 
-int procSimulation(int usernum){
-	ndict_Dict *userlist = setUserlist(usernum);
-	ndict_Dict *slotstate = setSlotstate(userlist);
-	ndict_print(userlist);
-	ndict_print(slotstate);
-	zigzag(userlist, slotstate);
-	int decoded = 0;
-	return decoded;
+void npoly_ver(){
+	double coef[4];
+	coef[3] = 4.0;
+	int coef_size = sizeof(coef)/sizeof(double);
+	nlist_List *poly = npoly_init(coef, coef_size, 1);
+	npoly_print(poly);
+	printf("%lf\n",npoly_subs(poly, 2.0));
+	npoly_integ(poly, 0.0, 1.0);
+	npoly_deriv(poly); npoly_print(poly);
 }
 
 int main(){
 	init_genrand((unsigned)time(NULL));
-
-	procSimulation(5);
-
+	list_ver();
+	dict_ver();
+	nrand_ver();
+	npoly_ver();
 	return 0;
 }
