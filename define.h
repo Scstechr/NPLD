d8e13887782c23469b3cbd92fd5ee8ced0de6541
Scstechr <teufelkonig@gmail.com>
2018-04-16 11:39:18 +0900

    define.h

--- define.h
+++ define.h
@@ -2,7 +2,7 @@
 #define DEFINE_H
 
 // may differ from machine to machine
-#define STACK_SIZE 10000// For MBA
+#define STACK_SIZE 20000// For MBA
 //#define STACK_SIZE 65536 // For Mac Mini
 
 #endif
